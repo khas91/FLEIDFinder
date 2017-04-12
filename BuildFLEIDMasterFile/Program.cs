856e215fa0fbd6a7353f10cfc85b7dcd523f40df
Stuart Pierson <spierson@fscj.edu>
2017-04-12 13:58:09 -0400

    This is a weird task that I just needed to automate It crawls all the files in its working directory to find the FLEIDs that were output by the state and compiles them into one file, or allows you to find FLEIDs for an input set of Student SSNs

---
+++ BuildFLEIDMasterFile/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FLEIDFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> outputRows = new List<string>();
+            List<String> studentIDs = new List<string>();
+            
+            foreach (String fileName in Directory.EnumerateFiles(".", "*.*", SearchOption.AllDirectories))
+            {
+                String[] pathComps = fileName.Split(new char[] { '\\' });
+
+                String name = pathComps[pathComps.Length - 1];
+
+                if (!(System.AppDomain.CurrentDomain.FriendlyName == name) && name.StartsWith("DPS"))
+                {
+                    foreach (String line in File.ReadAllLines(fileName))
+                    {
+                        String studentID = line.Substring(48, 10).Trim();
+
+                        if (line.Length < 575)
+                        {
+                            continue;
+                        }
+
+                        String FLEID = line.Substring(561, 14);
+
+                        if (FLEID.Trim() != "" && !studentIDs.Contains(studentID))
+                        {
+                            outputRows.Add(line);
+                            studentIDs.Add(studentID);
+                        }
+                    }
+                }
+            }
+
+            using (StreamWriter output = new StreamWriter("FLEID_Master.txt"))
+            {
+                foreach (String line in outputRows)
+                {
+                    output.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ FLEIDFinder/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FLEIDFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> studentIDs = File.ReadAllLines(args[0]).ToList();
+            Dictionary<String, String> FLEIDDict = new Dictionary<string, string>();
+            
+
+            foreach (String fileName in Directory.EnumerateFiles(".", "*.*", SearchOption.AllDirectories))
+            {
+                String[] pathComps = fileName.Split(new char[] { '\\' });
+
+                String name = pathComps[pathComps.Length - 1];
+
+                if (!(name == args[0]) && !(System.AppDomain.CurrentDomain.FriendlyName == name)
+                    && name.StartsWith("DPS"))
+                {
+                    foreach (String line in File.ReadAllLines(fileName))
+                    {
+                        String studentID = line.Substring(48, 10).Trim();
+
+                        if (line.Length < 575)
+                        {
+                            continue;
+                        }
+
+                        String FLEID = line.Substring(561, 14);
+
+                        if (studentIDs.Contains(studentID) && !FLEIDDict.ContainsKey(studentID) && FLEID.Trim() != "")
+                        {
+                            FLEIDDict.Add(studentID, FLEID);
+                        }
+                    }
+                }               
+            }
+
+            using (StreamWriter output = new StreamWriter("output.csv"))
+            {
+                foreach (String studentID in studentIDs)
+                {
+                    output.WriteLine("=\"" + studentID + "\"," + (FLEIDDict.ContainsKey(studentID) ? FLEIDDict[studentID] : "              "));
+                }
+            }
+        }
+    }
+}
